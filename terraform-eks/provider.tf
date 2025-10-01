5cd9c663dfca5979edbbe4d75bfabbcbc0671a3b
Diler <d1l3r@hotmail.com>
2025-10-01 20:10:17 +0100

    provider.tf update

--- terraform-eks/provider.tf
+++ terraform-eks/provider.tf
@@ -1,20 +1,14 @@
 terraform {
   required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.55"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
   region = var.aws_region
-}
-
-# Shared data sources
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-data "aws_availability_zones" "available" {
-  state = "available"
 }
